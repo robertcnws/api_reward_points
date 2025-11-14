aec674f75ca37a2872946d4ce88fdcaab372b663
Roberto C Gonzalez R <robertoc@newwindowsystem.com>
2025-11-14 10:43:46 -0500

    chore: Remove unnecessary blank line in reward itemgroups mock file

--- frontend_app/src/_mock/__reward-itemgroups.js
+++ frontend_app/src/_mock/__reward-itemgroups.js
@@ -28,7 +28,6 @@ export function useRewardItemgroupsById(groupId, fieldsDescriptor) {
   return { loading, error, data: value, refetch };
 }
 
-
 export function useRewardItemgroupsByZohoOrgId(zohoOrgId, fieldsDescriptor) {
   const QUERY = useMemo(() => {
     const selection = buildSelection(fieldsDescriptor).join('\n      ');
