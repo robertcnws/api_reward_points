27b34827c98b3c510ccc6c8e8f428e2bbd7ebc12
Roberto C Gonzalez R <robertoc@newwindowsystem.com>
2025-11-18 13:31:34 -0500

    feat: Refactor ECR lifecycle policy configuration for backend and frontend repositories

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -4,25 +4,27 @@ resource "aws_ecr_repository" "backend" {
   image_scanning_configuration {
     scan_on_push = true
   }
+}
 
-  lifecycle_policy {
-    policy = jsonencode({
-      rules = [
-        {
-          rulePriority = 1
-          description  = "Keep last 20 images"
-          selection = {
-            tagStatus   = "any"
-            countType   = "imageCountMoreThan"
-            countNumber = 20
-          }
-          action = {
-            type = "expire"
-          }
+resource "aws_ecr_lifecycle_policy" "backend" {
+  repository = aws_ecr_repository.backend.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep last 20 images"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 20
         }
-      ]
-    })
-  }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
 }
 
 resource "aws_ecr_repository" "frontend" {
@@ -31,25 +33,27 @@ resource "aws_ecr_repository" "frontend" {
   image_scanning_configuration {
     scan_on_push = true
   }
+}
 
-  lifecycle_policy {
-    policy = jsonencode({
-      rules = [
-        {
-          rulePriority = 1
-          description  = "Keep last 20 images"
-          selection = {
-            tagStatus   = "any"
-            countType   = "imageCountMoreThan"
-            countNumber = 20
-          }
-          action = {
-            type = "expire"
-          }
+resource "aws_ecr_lifecycle_policy" "frontend" {
+  repository = aws_ecr_repository.frontend.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep last 20 images"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 20
         }
-      ]
-    })
-  }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
 }
 
 locals {
